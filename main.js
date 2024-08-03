5934846d84f421aca9b1cf0bc156371374ae2134
FPG-992 <78109143+FPG-992@users.noreply.github.com>
2024-08-03 03:59:10 +0300

    Exercise Etch-a-Sketch done

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-
-function createGrid() {
-    
+function createGrid(size) {
+    document.documentElement.style.setProperty('--grid-size', size);
+    let grid = document.querySelector('.grid-container');
+    grid.innerHTML = ''; // Clear any existing cells
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let cell = document.createElement('div');
+            cell.className = 'cell';
+            cell.addEventListener('mouseover', () => {
+                cell.style.backgroundColor = 'black';
+            })
+            grid.appendChild(cell);
+        }
+    }
 }
+    
+const button = document.querySelector('#size');
+button.addEventListener('click', () => {
+    let size = prompt('Enter a grid size');
+    size = parseInt(size);
+    if (size>0 && size<=100){
+        createGrid(size);
+    } else {
+        alert('Please enter a number between 1 and 100');
+    }
+});
+
+createGrid(16);
